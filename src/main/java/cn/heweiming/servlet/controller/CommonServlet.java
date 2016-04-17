f7619a7429520d33d9ce2320c3fbaedaffd26d4a
heweiming <heweiming89@163.com>
2016-04-17 14:47:07 +0800

    test

---
+++ src/main/java/cn/heweiming/servlet/controller/CommonServlet.java
@@ -0,0 +1,36 @@
+package cn.heweiming.servlet.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.heweiming.servlet.domain.User;
+
+/**
+ * Servlet implementation class CommonServlet
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/common" })
+public class CommonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public CommonServlet() {
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		User user = new User();
+		user.setUsername("张三");
+		user.setPassword("张三的密码");
+		request.setAttribute("user", user);
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
---
+++ src/main/java/cn/heweiming/servlet/domain/User.java
@@ -0,0 +1,31 @@
+package cn.heweiming.servlet.domain;
+
+public class User {
+
+	private String username;
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + "]";
+	}
+}
+
+
+
